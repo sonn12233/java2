26dc750d52cf2a25f69a9519fec2131efa8cc4de
sonn12233 <wooseok040326@gmail.com>
2026-03-25 16:03:18 +0900

    Add files via upload

---
+++ bar.java
@@ -0,0 +1,19 @@
+public class bar {
+
+
+
+
+public static void main(String[] args){
+    final double PI = 3.14;
+    double radius =  10.2;
+    double circleArea = radius * radius * PI;
+
+
+    System.out.println("반지름" + radius + ", ");   
+    System.out.println("원의 변칙");
+    System.out.println("원의면적" + circleArea);
+
+}
+
+}
+
